ed01784f29abc67a7799929d289b38693d4e683f
antoniotrkdz <antoniotrkdz@gmail.com>
2017-07-05 16:14:28 +0100

    Setup Server.js and dependencies, moved index.html to public folder, small modification to db_build.sql Server has been setup with Hapi. It has hapi-auth and cookies in place + some other little things. index.html has been moved to public folder (just created) and modified to show a welcome h1. db_build.sql has been modified to include category in jobs table.

---
+++ src/server/server.js
@@ -0,0 +1,84 @@
+'use strict';
+const Hapi = require('hapi');
+//const routes = require('./routes');
+const _ = require('lodash');
+const pkg = require('../../package.json');
+const inert = require('inert');
+const h2o2 = require('h2o2');
+const vision = require('vision');
+const blipp = require('blipp');
+const cookieAuth = require('hapi-auth-cookie');
+const hapiAuth = require('hapi-auth-basic');
+const credentials = require('hapi-context-credentials');
+const fs = require('fs');
+const env = require('env2');
+env('./config.env');
+
+const server = new Hapi.Server();
+
+const PORT = process.env.PORT || 4000;
+
+server.connection({
+    port: PORT,
+    /*tls: process.env.NODE_ENV !== 'production' && {
+        key: fs.readFileSync('./keys/key.pem'),
+        cert: fs.readFileSync('./keys/cert.pem'),
+    },
+    state: {
+    isSameSite: 'Lax',
+    },*/
+});
+
+const plugins = [inert, vision, h2o2, blipp, hapiAuth, cookieAuth, credentials];
+    
+server.register(plugins, (err) => {
+  if (err) throw err;
+
+    console.log('=> Registered plugins:', {
+        plugins: _.keysIn(server.registrations).join(', ')
+    });
+    
+    const options = {
+        password: process.env.COOKIE_PASSWORD,
+        cookie: 'logged-in',
+        isSecure: false,
+        ttl: 24 * 60 * 60 * 1000,
+    };
+
+    server.auth.strategy('base', 'cookie', 'optional', options);
+
+    server.route({
+        method: 'GET',
+        path: '/{path*}',
+        handler: {
+            directory: {
+                path: './public',
+                listing: false,
+                index: true
+            }
+        }
+    });
+
+    // serve up some sample JSON data
+    server.route({
+        method: 'GET',
+        path: '/data',
+        handler: (request, reply) => {
+            reply({
+                name: pkg.name,
+                version: pkg.version,
+                message: 'Welcome to BEEVR!'
+            });
+        }
+    });
+
+    server.start(err => {
+        if (err) {
+        throw err;
+    }
+    
+    console.log(`=> BEEVR Server running at: ${server.info.uri}`);
+    });
+});
+
+module.exports = server;
