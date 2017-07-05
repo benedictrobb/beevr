3a2be316b0df686ad3f139a55ba898da63b731de
antoniotrkdz <antoniotrkdz@gmail.com>
2017-07-05 12:02:38 +0100

    Started the calendar date range picker component

---
+++ src/components/Calendar-range-picker.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react';
+import { DateRange } from "react-date-range';
+
+class DateRange extends Component {
+    handleSelect(range) {
+        console.log(range);
+    }
+
+        render() {
+            return (
+                <div>
+                    <DateRange
+                        onInit={this.handleSelect}
+                        onChange={this.handleSelect}
+                    />
+                </div>
+            )
+        }
+}
