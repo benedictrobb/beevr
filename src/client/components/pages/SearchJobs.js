6c6724ddb8f607cc9ece7abe3038b02d285cea20
Maja Kudlicka <maja.kudlicka@gmail.com>
2017-07-13 10:20:08 +0100

    displayed jobs on front end
    relates to #29

--- src/client/components/pages/SearchJobs.js
+++ src/client/components/pages/SearchJobs.js
@@ -1,8 +1,50 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import Axios from 'axios';
 
 class BrowseJobs extends Component {
+    constructor() {
+        super();
+        this.renderJobs = this.renderJobs.bind(this);
+        this.state = {
+            data: {}
+        };
+    }
+
+    componentWillMount() {
+        Axios.get('/api/jobs').then(response => {
+            console.log(response.data.jobsList);
+            var data = response.data;
+            this.setState({data});
+        });
+    }
+
+    renderJobs(job) {
+        return (
+            <div>
+                <h2>
+                    {job.category}
+                </h2>
+                <p>
+                    {' '}{job.description}{' '}
+                </p>
+                <label>Start Date</label>
+                <p>
+                    {job.start_date}
+                </p>
+                <label>Start Time</label>
+                <p>
+                    {job.start_time}
+                </p>
+            </div>
+        );
+    }
     render() {
+        let data = this.state.data.jobsList;
+
+        if (!data) {
+            return <div>Loading</div>;
+        }
         return (
             <article>
                 <section className="text-section">
@@ -14,6 +56,9 @@ class BrowseJobs extends Component {
                         id="Browse Jobs"
                         placeholder="Browse Jobs"
                     />
+                    <ul>
+                        {data.map(this.renderJobs)}
+                    </ul>
                 </section>
             </article>
         );
