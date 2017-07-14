11537de996d42124b088239233166a9315a18e17
Maja Kudlicka <maja.kudlicka@gmail.com>
2017-07-14 11:04:10 +0100

    created search jobs reducer
    relates to #45

--- src/client/reducers/index.js
+++ src/client/reducers/index.js
@@ -1,7 +1,7 @@
 import {combineReducers} from 'redux';
 
 import home from './home';
-import studentDashboard from './search_jobs_reducer.js';
+import search_jobs_reducer from './search_jobs_reducer.js';
 
 export default combineReducers({
     home,
