1ff151c43344d20c0a8f676b6bbc044875e70971
Maja Kudlicka <maja.kudlicka@gmail.com>
2017-07-20 12:49:43 +0100

    added a function that sends an email
    relates to #58

--- react-ui/src/components/pages/JobDetail.js
+++ react-ui/src/components/pages/JobDetail.js
@@ -1,6 +1,83 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import * as actions from '../../actions/search_jobs.js';
+var aws = require('aws-sdk');
+const env = require('env2');
+env('../../../../config.env');
+
+console.log(process.env.SES_ACCESS_ID);
+
+aws.config = {
+    accessKeyId: 'id',
+    secretAccessKey: 'key',
+    region: 'eu-west-1'
+};
+
+// load aws sdk
+
+// load aws config
+// aws.config.loadFromPath('config.json');
+
+// load AWS SES
+var ses = new aws.SES({apiVersion: '2010-12-01'});
+
+// send to list
+var to = ['rmrajaa@gmail.com'];
+
+// this must relate to a verified SES account
+var from = 'maja.kudlicka@gmail.com';
+
+// this sends the email
+// @todo - add HTML version
+ses.sendEmail(
+    {
+        Source: from,
+        Destination: {ToAddresses: to},
+        Message: {
+            Subject: {
+                Data: 'A Message To You Rudy'
+            },
+            Body: {
+                Text: {
+                    Data: 'Stop your messing around'
+                }
+            }
+        }
+    },
+    function(err, data) {
+        if (err) throw err;
+        console.log('Email sent:');
+        console.log(data);
+    }
+);
+
+// var key = process.env.SES_ACCESS_ID;
+// var secret = process.env.SES_ACCESS_KEY;
+//
+// var ses = require('node-ses'),
+//     client = ses.createClient({
+//         key: 'AKIAJ3QGMLKZUSI3HT6A',
+//         secret: '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',
+//         amazon: 'https://email.eu-west-1.amazonaws.com'
+//     });
+
+// // Give SES the details and let it construct the message for you.
+// client.sendEmail(
+//     {
+//         to: 'rmrajaa@gmail.com',
+//         from: 'maja.kudlicka@gmail.com',
+//         subject: 'greetings',
+//         message: 'your <b>message</b> goes here',
+//         altText: 'plain text'
+//     },
+//     function(err, data, res) {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             console.log('email sent!');
+//         }
+//     }
+// );
 
 class JobDetail extends Component {
     constructor() {
