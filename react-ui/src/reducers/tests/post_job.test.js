4c666efdff8e29febb3db4d50e00a29f8545bce9
Maja Kudlicka <maja.kudlicka@gmail.com>
2017-07-26 12:03:03 +0100

    added more tests to post job reducer

--- react-ui/src/reducers/tests/post_job.test.js
+++ react-ui/src/reducers/tests/post_job.test.js
@@ -8,4 +8,80 @@ describe('post job reducer', () => {
             jobData: {}
         });
     });
+
+    it('should handle POST_JOB', () => {
+        expect(
+            reducer(
+                {},
+                {
+                    type: types.POST_JOB,
+                    status: 'pending'
+                }
+            )
+        ).toEqual({
+            newJob: {
+                status: 'pending',
+                error: undefined,
+                response: undefined
+            }
+        });
+    });
+
+    it('should handle POST_JOB', () => {
+        expect(
+            reducer(
+                {},
+                {
+                    type: types.POST_JOB,
+                    status: 'success',
+                    response: {
+                        start_date: '01/12/90',
+                        start_time: '12:00',
+                        end_date: '12/12/12',
+                        end_time: '07:00',
+                        job_title: 'Hello Chello',
+                        description: 'Lorem Ipsum',
+                        rate: '100',
+                        resident_id: '1',
+                        category: 'photography'
+                    }
+                }
+            )
+        ).toEqual({
+            newJob: {
+                status: 'success',
+                error: undefined,
+                response: {
+                    start_date: '01/12/90',
+                    start_time: '12:00',
+                    end_date: '12/12/12',
+                    end_time: '07:00',
+                    job_title: 'Hello Chello',
+                    description: 'Lorem Ipsum',
+                    rate: '100',
+                    resident_id: '1',
+                    category: 'photography'
+                }
+            }
+        });
+    });
+
+    it('should handle POST_JOB', () => {
+        expect(
+            reducer(
+                {},
+                {
+                    type: types.POST_JOB,
+                    status: 'error',
+                    response: error
+                }
+            )
+        ).toEqual({
+            newJob: {
+                status: 'error',
+                error: error,
+                response: undefined
+            }
+        });
+    });
 });
