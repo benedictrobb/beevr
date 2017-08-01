047eaae906acf86e367824fa7859d8e3c7d005b8
Maja Kudlicka <maja.kudlicka@gmail.com>
2017-08-01 14:10:47 +0300

    removed console.log and fixing bugs on master

--- server/index.js
+++ server/index.js
@@ -18,325 +18,312 @@ const server = new Hapi.Server();
 const PORT = process.env.PORT || 4000;
 
 server.connection({
-    port: PORT,
+  port: PORT
 });
 
 const plugins = [inert, blipp, cookieAuth];
 
 server.register(plugins, err => {
-    if (err) throw err;
+  if (err) throw err;
 
-    console.log('=> Registered plugins:', {
-        plugins: _.keysIn(server.registrations).join(', '),
-    });
+  console.log('=> Registered plugins:', {
+    plugins: _.keysIn(server.registrations).join(', ')
+  });
 
-    const cookieAuthOptions = {
-        password: process.env.COOKIE_PASSWORD,
-        cookie: 'loggedIn',
-        isSecure: false,
-        ttl: 24 * 60 * 60 * 1000,
-    };
+  const cookieAuthOptions = {
+    password: process.env.COOKIE_PASSWORD,
+    cookie: 'loggedIn',
+    isSecure: false,
+    ttl: 24 * 60 * 60 * 1000
+  };
 
-    server.auth.strategy('session', 'cookie', 'optional', cookieAuthOptions);
+  server.auth.strategy('session', 'cookie', 'optional', cookieAuthOptions);
 
-    server.route({
-        method: 'GET',
-        path: '/{path*}',
-        handler: {
-            directory: {
-                path: './react-ui/build',
-                listing: false,
-                index: true,
-            },
-        },
-    });
+  server.route({
+    method: 'GET',
+    path: '/{path*}',
+    handler: {
+      directory: {
+        path: './react-ui/build',
+        listing: false,
+        index: true
+      }
+    }
+  });
 
-    server.route({
-        method: 'GET',
-        path: '/api/jobs',
-        handler: (request, reply) => {
-          console.log('inside server')
-            data.getJobs((err, res) => {
-                if (err) {
-                    return reply(Boom.serverUnavailable('unavailable: ' + err));
-                }
-                reply({
-                    name: 'jobsList',
-                    message: 'Welcome to BEEVR!',
-                    jobsList: res.map(element => {
-                        return {
-                            job_id: element.job_id,
-                            job_title: element.job_title,
-                            start_date: element.start_date,
-                            start_time: element.start_time,
-                            end_date: element.end_date,
-                            end_time: element.end_time,
-                            description: element.description,
-                            category: element.category,
-                            rate: element.rate,
-                            student_id: element.student_id,
-                            resident_id: element.resident_id,
-                        };
-                    }),
-                });
-            }, request.url.query.term);
-        },
-    });
-
-    server.route({
-        method: 'POST',
-        path: '/api/jobs',
-        handler: (request, reply) => {
-            data.postJobs(request.payload, (err, res) => {
-                if (err) {
-                    return reply(Boom.badRequest('Bad request: ' + err));
-                }
-                reply({
-                    name: 'newJob',
-                    message: 'Job posted succesfully!',
-                });
-            });
-        },
-    });
-
-    server.route({
-        method: 'GET',
-        path: '/api/get-students',
-        handler: (request, reply) => {
-            data.getStudents((err, res) => {
-                if (err) {
-                    return reply(Boom.serverUnavailable('unavailable: ' + err));
-                } else {
-                    reply({
-                        name: 'studentList',
-                        message: 'Welcome to BEEVR!',
-                        studentList: res.map(element => {
-                            return {
-                                studentId: element.student_id,
-                                firstName: element.first_name,
-                                lastName: element.last_name,
-                                dob: element.dob,
-                                univSchool: element.univ_school,
-                                bio: element.bio,
-                                picture: element.picture,
-                                jobCat: element.job_cat,
-                            };
-                        }),
-                    });
-                }
-            }, request.url.query.searchTerm);
-        },
-    });
-
-    server.route({
-        method: 'POST',
-        path: '/api/student',
-        handler: (request, reply) => {
-            data.studentExists(request.payload.email, (err, res) => {
-                if (err) {
-                    return reply(Boom.badRequest('Bad request: ' + err));
-                }
-                reply(res.exists);
-            });
-        },
-    });
-
-    server.route({
-        method: 'POST',
-        path: '/api/reg-student',
-        handler: (request, reply) => {
-            hashPassword(request.payload.password, (err, hash) => {
-                if (err) {
-                    return reply(Boom.badData('bcrypt error'));
-                }
-                data.postStudents(
-                    Object.assign({}, request.payload, {passwordHash: hash}),
-                    (err, res) => {
-                        if (err) {
-                            return reply(
-                                Boom.badRequest('Bad request: ' + err)
-                            );
-                        }
-                        reply({
-                            name: 'student',
-                            status: 'success',
-                            message: `Registration successful!
-                            Welcome to BEEVR!`,
-                        });
-                    }
-                );
-            });
-        },
-    });
-
-    server.route({
-        method: 'POST',
-        path: '/api/resident',
-        handler: (request, reply) => {
-            data.residentExists(request.payload.email, (err, res) => {
-                if (err) {
-                    return reply(Boom.badRequest('Bad request: ' + err));
-                }
-                reply(res.exists);
-            });
-        },
-    });
-
-    server.route({
-        method: 'POST',
-        path: '/api/reg-resident',
-        handler: (request, reply) => {
-            hashPassword(request.payload.password, (err, hash) => {
-                if (err) {
-                    return reply(Boom.badData('bcrypt error'));
-                }
-                data.postResidents(
-                    Object.assign({}, request.payload, {passwordHash: hash}),
-                    (err, res) => {
-                        if (err) {
-                            return reply(
-                                Boom.badRequest('Bad request: ' + err)
-                            );
-                        }
-                        reply({
-                            name: 'resident',
-                            status: 'success',
-                            message: `Registration successful!
-                            Welcome to BEEVR!`,
-                        });
-                    }
-                );
-            });
-        },
-    });
-
-    //unhardcode the job id
-    server.route({
-        method: 'PUT',
-        path: '/api/apply',
-        handler: (request, reply) => {
-            var to = ['rmrajaa@gmail.com'];
-            var from = 'maja.kudlicka@gmail.com';
-            var subject = 'New job application';
-            var text =
-                'Someone has applied for the job you posted. Go to your profile to find out more.';
-            data.submitApplication(request.payload.job_id, (err, res) => {
-                if (err) {
-                  reply(
-                      Boom.serverUnavailable(
-                          'Failed to retrieve data from database'
-                      )
-                    );
-                } else {
-                    sendEmail(from, to, subject, text, (err, res) => {
-                        if (err) {
-                          reply(Boom.internal('Failed to send email', 500));
-                        } else {
-                            reply({
-                                name: 'applyJob',
-                                message: 'Email sent!',
-                                applyJob: res
-                            });
-                        }
-                    });
-                }
-            });
-        },
-    });
-
-    server.route({
-        method: 'POST',
-        path: '/api/auth',
-        handler: (request, reply) => {
-            console.log(request.payload);
-            const email = request.payload.email;
-            data.loginRequest(email, (err, res) => {
-                if (err) {
-                    return reply(
-                        Boom.unauthorized('Please log-in to see that')
-                    );
-                }
-                const user = res;
-                comparePassword(
-                    request.payload.password,
-                    user.password_hash,
-                    (err, match) => {
-                        if (err) {
-                            return reply(
-                                Boom.unauthorized('Please log-in to see that')
-                            );
-                        }
-                        request.cookieAuth.set({email});
-                        reply({
-                            name: 'loginRequest',
-                            message: 'Welcome to BEEVR!',
-                            status: 'success',
-                            isAuthenticated: true,
-                            id: res.id,
-                            role: res.role,
-                        });
-                    }
-                );
-            });
-        },
-    });
-
-    //student is hardcoded to 2 until we have session management capacity
-    server.route({
-        method: 'GET',
-        path: '/api/myjobs',
-        handler: (request, reply) => {
-            data.getMyJobs((err, res) => {
-                if (err) {
-                    reply(
-                        Boom.serverUnavailable(
-                            'Failed to retrieve data from database'
-                        )
-                    );
-                } else {
-                    reply({
-                        name: 'myJobsList',
-                        message: 'Welcome to BEEVR!',
-                        myJobsList: res.map(element => {
-                            return {
-                                jobId: element.job_id,
-                                jobTitle: element.job_title,
-                                startDate: element.start_date,
-                                startTime: element.start_time,
-                                endDate: element.end_date,
-                                endTime: element.end_time,
-                                description: element.description,
-                                jobCat: element.category,
-                                rate: element.rate,
-                                studentId: element.student_id,
-                                residentId: element.resident_id,
-                            };
-                        }),
-                    });
-                }
-            }, 2);
-        },
-    });
-
-    server.route({
-        method: 'GET',
-        path: '/api',
-        handler: (request, reply) => {
-            reply({
-                name: pkg.name,
-                version: pkg.version,
-                message: 'Welcome to BEEVR!',
-            });
-        },
-    });
-
-    server.start(err => {
+  server.route({
+    method: 'GET',
+    path: '/api/jobs',
+    handler: (request, reply) => {
+      data.getJobs((err, res) => {
         if (err) {
-            throw err;
+          return reply(Boom.serverUnavailable('unavailable: ' + err));
         }
+        reply({
+          name: 'jobsList',
+          message: 'Welcome to BEEVR!',
+          jobsList: res.map(element => {
+            return {
+              job_id: element.job_id,
+              job_title: element.job_title,
+              start_date: element.start_date,
+              start_time: element.start_time,
+              end_date: element.end_date,
+              end_time: element.end_time,
+              description: element.description,
+              category: element.category,
+              rate: element.rate,
+              student_id: element.student_id,
+              resident_id: element.resident_id
+            };
+          })
+        });
+      }, request.url.query.term);
+    }
+  });
 
-        console.log(`=> BEEVR Server running at: ${server.info.uri}`);
-    });
+  server.route({
+    method: 'POST',
+    path: '/api/jobs',
+    handler: (request, reply) => {
+      data.postJobs(request.payload, (err, res) => {
+        if (err) {
+          return reply(Boom.badRequest('Bad request: ' + err));
+        }
+        reply({
+          name: 'newJob',
+          message: 'Job posted succesfully!'
+        });
+      });
+    }
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/api/get-students',
+    handler: (request, reply) => {
+      data.getStudents((err, res) => {
+        if (err) {
+          return reply(Boom.serverUnavailable('unavailable: ' + err));
+        } else {
+          reply({
+            name: 'studentList',
+            message: 'Welcome to BEEVR!',
+            studentList: res.map(element => {
+              return {
+                student_id: element.student_id,
+                first_name: element.first_name,
+                last_name: element.last_name,
+                dob: element.dob,
+                univ_school: element.univ_school,
+                bio: element.bio,
+                picture: element.picture,
+                job_cat: element.job_cat
+              };
+            })
+          });
+        }
+      }, request.url.query.searchTerm);
+    }
+  });
+
+  server.route({
+    method: 'POST',
+    path: '/api/student',
+    handler: (request, reply) => {
+      data.studentExists(request.payload.email, (err, res) => {
+        if (err) {
+          return reply(Boom.badRequest('Bad request: ' + err));
+        }
+        reply(res.exists);
+      });
+    }
+  });
+
+  server.route({
+    method: 'POST',
+    path: '/api/reg-student',
+    handler: (request, reply) => {
+      hashPassword(request.payload.password, (err, hash) => {
+        if (err) {
+          return reply(Boom.badData('bcrypt error'));
+        }
+        data.postStudents(
+          Object.assign({}, request.payload, {passwordHash: hash}),
+          (err, res) => {
+            if (err) {
+              return reply(Boom.badRequest('Bad request: ' + err));
+            }
+            reply({
+              name: 'student',
+              status: 'success',
+              message: `Registration successful!
+                            Welcome to BEEVR!`
+            });
+          }
+        );
+      });
+    }
+  });
+
+  server.route({
+    method: 'POST',
+    path: '/api/resident',
+    handler: (request, reply) => {
+      data.residentExists(request.payload.email, (err, res) => {
+        if (err) {
+          return reply(Boom.badRequest('Bad request: ' + err));
+        }
+        reply(res.exists);
+      });
+    }
+  });
+
+  server.route({
+    method: 'POST',
+    path: '/api/reg-resident',
+    handler: (request, reply) => {
+      hashPassword(request.payload.password, (err, hash) => {
+        if (err) {
+          return reply(Boom.badData('bcrypt error'));
+        }
+        data.postResidents(
+          Object.assign({}, request.payload, {passwordHash: hash}),
+          (err, res) => {
+            if (err) {
+              return reply(Boom.badRequest('Bad request: ' + err));
+            }
+            reply({
+              name: 'resident',
+              status: 'success',
+              message: `Registration successful!
+                            Welcome to BEEVR!`
+            });
+          }
+        );
+      });
+    }
+  });
+
+  //unhardcode the job id
+  server.route({
+    method: 'PUT',
+    path: '/api/apply',
+    handler: (request, reply) => {
+      var to = ['rmrajaa@gmail.com'];
+      var from = 'maja.kudlicka@gmail.com';
+      var subject = 'New job application';
+      var text =
+        'Someone has applied for the job you posted. Go to your profile to find out more.';
+      data.submitApplication(request.payload.job_id, (err, res) => {
+        if (err) {
+          reply(
+            Boom.serverUnavailable('Failed to retrieve data from database')
+          );
+        } else {
+          sendEmail(from, to, subject, text, (err, res) => {
+            if (err) {
+              reply(Boom.internal('Failed to send email', 500));
+            } else {
+              reply({
+                name: 'applyJob',
+                message: 'Email sent!',
+                applyJob: res
+              });
+            }
+          });
+        }
+      });
+    }
+  });
+
+  server.route({
+    method: 'POST',
+    path: '/api/auth',
+    handler: (request, reply) => {
+      console.log(request.payload);
+      const email = request.payload.email;
+      data.loginRequest(email, (err, res) => {
+        if (err) {
+          return reply(Boom.unauthorized('Please log-in to see that'));
+        }
+        const user = res;
+        comparePassword(
+          request.payload.password,
+          user.password_hash,
+          (err, match) => {
+            if (err) {
+              return reply(Boom.unauthorized('Please log-in to see that'));
+            }
+            request.cookieAuth.set({email});
+            reply({
+              name: 'loginRequest',
+              message: 'Welcome to BEEVR!',
+              status: 'success',
+              isAuthenticated: true,
+              id: res.id,
+              role: res.role
+            });
+          }
+        );
+      });
+    }
+  });
+
+  //student is hardcoded to 2 until we have session management capacity
+  server.route({
+    method: 'GET',
+    path: '/api/myjobs',
+    handler: (request, reply) => {
+      data.getMyJobs((err, res) => {
+        if (err) {
+          reply(
+            Boom.serverUnavailable('Failed to retrieve data from database')
+          );
+        } else {
+          reply({
+            name: 'myJobsList',
+            message: 'Welcome to BEEVR!',
+            myJobsList: res.map(element => {
+              return {
+                jobId: element.job_id,
+                jobTitle: element.job_title,
+                startDate: element.start_date,
+                startTime: element.start_time,
+                endDate: element.end_date,
+                endTime: element.end_time,
+                description: element.description,
+                jobCat: element.category,
+                rate: element.rate,
+                studentId: element.student_id,
+                residentId: element.resident_id
+              };
+            })
+          });
+        }
+      }, 2);
+    }
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/api',
+    handler: (request, reply) => {
+      reply({
+        name: pkg.name,
+        version: pkg.version,
+        message: 'Welcome to BEEVR!'
+      });
+    }
+  });
+
+  server.start(err => {
+    if (err) {
+      throw err;
+    }
+
+    console.log(`=> BEEVR Server running at: ${server.info.uri}`);
+  });
 });
 
 module.exports = server;
