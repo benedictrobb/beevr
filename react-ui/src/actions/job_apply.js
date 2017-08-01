041d756694ac0c4cdfd2cf96eb848c8bbf71b3de
Maja Kudlicka <maja.kudlicka@gmail.com>
2017-08-01 16:08:20 +0300

    fixing indentation

--- react-ui/src/actions/job_apply.js
+++ react-ui/src/actions/job_apply.js
@@ -9,18 +9,18 @@ export const submitJobApplication = job_id => dispatch => {
   axios
     .get('/api/apply')
     .then(response => {
-        dispatch({
-            type: APPLY_JOB,
-            status: 'success',
-            response: job_id
-        });
+      dispatch({
+          type: APPLY_JOB,
+          status: 'success',
+          response: job_id
+      });
     })
     .catch(error => {
-        dispatch({
-            type: APPLY_JOB,
-            status: 'error',
-            error: error
-        });
+      dispatch({
+          type: APPLY_JOB,
+          status: 'error',
+          error: error
+      });
     });
 };
 
