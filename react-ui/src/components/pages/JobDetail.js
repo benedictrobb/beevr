b5dbb948d410aef692a4ebffc538257bc5bbe9c6
Maja Kudlicka <maja.kudlicka@gmail.com>
2017-08-01 13:12:57 +0300

    importing loading indicator as module

--- react-ui/src/components/pages/JobDetail.js
+++ react-ui/src/components/pages/JobDetail.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import * as actions from '../../actions/job_apply.js';
-import LoadingIndicator from '../../utils/loading_indicator.js';
+import LoadingIndicator from 'react-loading-indicator';
 
 class JobDetail extends Component {
     constructor() {
