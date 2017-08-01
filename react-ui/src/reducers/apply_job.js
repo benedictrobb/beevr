c0163a06ba82413fd2e8deba4dc0aeb49f7da9dd
Maja Kudlicka <maja.kudlicka@gmail.com>
2017-08-01 16:26:04 +0300

    correcting merge

--- react-ui/src/reducers/apply_job.js
+++ react-ui/src/reducers/apply_job.js
@@ -2,25 +2,18 @@ import {APPLY_JOB, FETCH_JOBS} from '../constants/action_types.js';
 
 const initialState = {
     applied: [],
-    isLoading= false,
+    isLoading: false,
     jobsRequest: {},
 };
 
 export default (state = initialState, action) => {
     switch (action.type) {
     case APPLY_JOB:
-        return {
-            ...state,
-            status: action.status,
-            error: action.error,
-            applied: [...state.applied, action.response],
-        };
-    case FETCH_JOBS:
         if (action.status === 'pending') {
             return {
                 ...state,
                 status: action.status,
-                isLoading: true,
+                isLoading: true
             };
         } else {
             return {
@@ -28,9 +21,19 @@ export default (state = initialState, action) => {
                 status: action.status,
                 error: action.error,
                 applied: [...state.applied, action.response],
-                isLoading: false,
+                isLoading: false
             };
         }
+
+    case FETCH_JOBS:
+    return {
+        ...state,
+        jobsRequest: {
+            status: action.status,
+            error: action.error,
+            response: action.response
+        }
+    };
     default:
         return state;
     }
