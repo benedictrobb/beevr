c68d7fed33856e16bcea2af3690afbed058bd724
Maja Kudlicka <maja.kudlicka@gmail.com>
2017-08-01 18:50:05 +0300

    Update db_queries.js

--- server/database/db_queries.js
+++ server/database/db_queries.js
@@ -18,9 +18,9 @@ data.getJobs = (callback, term) => {
         );
     } else {
         dbConnection.query(
-            `SELECT * FROM jobs
-                WHERE CATEGORY = $1
-                AND end_date < NOW()
+            `SELECT * FROM jobs 
+                WHERE CATEGORY = $1 
+                AND end_date < NOW() 
                 ORDER BY start_date;`,
             [term],
             (err, res) => {
@@ -36,16 +36,16 @@ data.getJobs = (callback, term) => {
 data.getStudents = (callback, term) => {
     if (!term) {
         dbConnection.query(
-            `SELECT
-                students.student_id,
-                students.first_name,
-                students.last_name,
-                students.email,
-                students.dob,
-                students.univ_school,
-                students.bio,
-                students.picture,
-                students.phone,
+            `SELECT 
+                students.student_id, 
+                students.first_name, 
+                students.last_name, 
+                students.email, 
+                students.dob, 
+                students.univ_school, 
+                students.bio, 
+                students.picture, 
+                students.phone, 
                 students.job_cat
                     FROM students LIMIT 10;`,
             (err, res) => {
@@ -58,17 +58,17 @@ data.getStudents = (callback, term) => {
         );
     } else {
         dbConnection.query(
-            `SELECT
-                students.student_id,
-                students.first_name,
-                students.last_name,
-                students.email,
-                students.dob,
-                students.univ_school,
-                students.bio,
-                students.picture,
-                students.phone,
-                students.job_cat
+            `SELECT 
+                students.student_id, 
+                students.first_name, 
+                students.last_name, 
+                students.email, 
+                students.dob, 
+                students.univ_school, 
+                students.bio, 
+                students.picture, 
+                students.phone, 
+                students.job_cat 
                     FROM students WHERE $1 = ANY (job_cat);`,
             [term],
             (err, res) => {
@@ -84,14 +84,14 @@ data.getStudents = (callback, term) => {
 
 data.loginRequest = (email, callback) => {
     dbConnection.query(
-        `SELECT residents.resident_id AS id,
-        residents.email, residents.password_hash,
+        `SELECT residents.resident_id AS id, 
+        residents.email, residents.password_hash, 
         'Resident' AS role
             FROM residents
             WHERE residents.email = $1
             UNION ALL
-            SELECT students.student_id AS id,
-            students.email, students.password_hash,
+            SELECT students.student_id AS id, 
+            students.email, students.password_hash, 
             'Student' AS role
                 FROM students
                 WHERE students.email = $1;`,
@@ -136,7 +136,7 @@ data.studentExists = (email, callback) => {
 data.postStudents = (student, callback) => {
     dbConnection.query(
         `INSERT INTO students(
-            first_name, last_name, email, DOB, univ_school,
+            first_name, last_name, email, DOB, univ_school, 
             bio, picture, phone, job_cat, password_hash)
             VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);`,
         [
@@ -178,8 +178,8 @@ data.residentExists = (email, callback) => {
 data.postResidents = (resident, callback) => {
     dbConnection.query(
         `INSERT INTO residents(
-            first_name, last_name, email, DOB,
-            address, bio, picture, phone, password_hash)
+            first_name, last_name, email, DOB, 
+            address, bio, picture, phone, password_hash) 
             VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9);`,
         [
             resident.firstName,
@@ -204,8 +204,8 @@ data.postResidents = (resident, callback) => {
 data.postJobs = (jobsObject, callback) => {
     dbConnection.query(
         `INSERT INTO jobs(
-        job_title, start_date, start_time, end_date,
-        end_time, description, rate, resident_id, category)
+        job_title, start_date, start_time, end_date, 
+        end_time, description, rate, resident_id, category) 
         VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9);`,
         [
             jobsObject.job_title,
@@ -226,20 +226,6 @@ data.postJobs = (jobsObject, callback) => {
             callback(null, res.rows);
         }
     );
-};
-
-data.deleteApplication = (job_id, callback) => {
-  dbConnection.query(
-    'UPDATE jobs SET student_id = array_remove(student_id, \'2\') WHERE job_id = $1',
-    [job_id],
-    (err, res) => {
-      if (err) {
-        callback(err);
-      } else {
-        callback(null, res);
-      }
-    }
-  );
 };
 
 module.exports = data;
