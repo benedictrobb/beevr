373226d9cc09068b21f75710ae024dd6a6409996
Maja Kudlicka <maja.kudlicka@gmail.com>
2017-08-01 15:17:19 +0300

    resolving merge conflicts

--- react-ui/src/actions/job_apply.js
+++ react-ui/src/actions/job_apply.js
@@ -4,28 +4,22 @@ import axios from 'axios';
 export const submitJobApplication = job_id => dispatch => {
   dispatch({
     type: APPLY_JOB,
-    status: 'pending'
+    status: 'pending',
   });
   axios
-<<<<<<< HEAD
-    .put('/api/apply',{'job_id': job_id})
-=======
-    .get('/api/apply', {
-            params: {job_id}
-        })
->>>>>>> ce72015bf62b41fe41e63c8a94e04452278c040e
+    .put('/api/apply', {job_id: job_id})
     .then(response => {
       dispatch({
         type: APPLY_JOB,
         status: 'success',
-        response: job_id
+        response: job_id,
       });
     })
     .catch(error => {
       dispatch({
         type: APPLY_JOB,
         status: 'error',
-        error: error
+        error: error,
       });
     });
 };
