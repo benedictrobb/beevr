f4898d93419f5a09d7a09c02052f1800cd8a49b8
Maja Kudlicka <maja.kudlicka@gmail.com>
2017-08-06 18:29:48 +0300

    made burger menu to work
    relates to #102

--- react-ui/src/components/Nav.js
+++ react-ui/src/components/Nav.js
@@ -4,15 +4,16 @@ import {Link} from 'react-router';
 class Nav extends Component {
     render() {
         return (
-            <nav className="navbar navbar-default">
+            <nav className="avbar navbar-default" role="navigation">
                 <div className="container">
                     <div className="navbar-header">
                         <button
                             type="button"
-                            className="navbar-toggle"
+                            className="navbar-toggle collapsed"
                             data-toggle="collapse"
-                            data-target="#myNavbar"
+                            data-target="#bs-example-navbar-collapse-1"
                         >
+                            <span className="sr-only">Toggle navigation</span>
                             <span className="icon-bar" />
                             <span className="icon-bar" />
                             <span className="icon-bar" />
@@ -21,26 +22,26 @@ class Nav extends Component {
                             Login
                         </Link>
                     </div>
-                    <div id="myNavbar">
-                        <div className="collapse navbar-collapse" id="myNavbar">
-                            <ul className="nav navbar-nav navbar-right">
-                                <li>
-                                    {' '}<Link to="/">Home</Link>{' '}
-                                </li>
 
-                                <li>
-                                    {' '}<Link to="/registerstudent">
-                                        Register As Student
-                                    </Link>{' '}
-                                </li>
-
-                                <li>
-                                    {' '}<Link to="/registerresident">
-                                        Register As Resident
-                                    </Link>
-                                </li>
-                            </ul>
-                        </div>
+                    <div
+                        className="collapse navbar-collapse"
+                        id="bs-example-navbar-collapse-1"
+                    >
+                        <ul className="nav navbar-nav navbar-right">
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/registerstudent">
+                                    Register As Student
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/registerresident">
+                                    Register As Resident
+                                </Link>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </nav>
