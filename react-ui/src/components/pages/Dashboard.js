015c803882539fdf442377015340e1541ead27f0
Maja Kudlicka <maja.kudlicka@gmail.com>
2017-08-27 09:51:00 +0100

    redesign of the dashboard

--- react-ui/src/components/pages/Dashboard.js
+++ react-ui/src/components/pages/Dashboard.js
@@ -5,19 +5,19 @@ class Dashboard extends Component {
     render() {
         return (
             <article>
-                <section className="container text-center">
+                <section className="container text-center bgimg_student">
                     <div className="welcome">
                         <h2>
                             <b>A JOBS PLATFORM FOR STUDENTS & RESIDENTS</b>
                         </h2>
                     </div>
-                    <div className="container-flex-dashboard">
+                    <div className="container-flex-dashboard button-group">
                         <div>
                             <Link
                                 to="/browsejobs"
                                 className="btn btn-primary front-button"
                             >
-                                Student
+                                <div className="button-text">Student</div>
                             </Link>
                         </div>
                         <div className="col-sm-1" />
@@ -26,7 +26,16 @@ class Dashboard extends Component {
                                 to="/browsestudents"
                                 className="btn btn-primary front-button"
                             >
-                                Resident
+                                <div className="button-text">Resident</div>
+                            </Link>
+                        </div>
+                        <div className="col-sm-1" />
+                        <div>
+                            <Link
+                                to="/login"
+                                className="btn btn-primary front-button sign-in-button"
+                            >
+                                <div className="button-text"> Sign In </div>
                             </Link>
                         </div>
                     </div>
