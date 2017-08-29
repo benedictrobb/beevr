2872256398aefd6c767990a1c14aa94b8017b5bd
antoniotrkdz <antoniotrkdz@gmail.com>
2017-08-29 13:36:39 +0100

    Fixed back-end

--- server/index.js
+++ server/index.js
@@ -220,23 +220,20 @@ server.register(plugins, err => {
                 mode: 'optional',
             },
             handler: (request, reply) => {
+                if (request.auth.credentials) {
+                    var studentId = request.auth.credentials.id;
+                } else var studentId = null;
                 hashPassword(request.payload.password, (err, hash) => {
                     if (err) {
                         return reply(Boom.badData('bcrypt error'));
                     }
-                    if (
-                        request &&
-                        request.auth &&
-                        request.auth.credentials &&
-                        request.auth.credentials.id
-                    ) {
-                        var studentId = request.auth.credentials.id;
-                    }
-                    var jobCategories =
-                        request.payload.jobCategories ||
-                        request.payload.jobCategories[0].map(
+                    if (studentId) {
+                        var jobCategories = request.payload.jobCategories;
+                    } else {
+                        var jobCategories = request.payload.jobCategories[0].map(
                             item => item.value
                         );
+                    }
                     data.postStudents(
                         studentId,
                         Object.assign({}, request.payload, {
